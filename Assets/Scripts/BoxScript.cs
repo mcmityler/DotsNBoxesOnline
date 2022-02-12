3876e876955a7243e27adbf0ca75b9e5829f180e
mcmityler <mcmityler@gmail.com>
2022-02-12 14:57:19 -0500

    V0.0.5
    Changelog:
    - Added the turns changing
    - Added the box centers changing color depending on who surrounds it first

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoxScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public List<Button> buttonList = new List<Button>(); //List of what buttons this box is surrounded by is passed to it through the creation of the box.
+    public void boxLines(Button _button){ //function to add buttons to list of buttons surrounding this box.
+        buttonList.Add(_button);
     }
+    public void ButtonSurrounded(bool _fpTurn){
+        //change obj tag
+        gameObject.tag = "checked";
+        //change colour of obj depending on players turn
+        if(_fpTurn){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            gameObject.GetComponent<Image>().color = Color.blue;
+        }
+        else if(!_fpTurn){
+
+            gameObject.GetComponent<Image>().color = Color.red;
+        }
     }
 }
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -91,18 +91,20 @@ public class GameScript : MonoBehaviour
                     //set buttons position on canvas
                     _rectTransform.position = new Vector2(_backgroundPanel.GetComponent<RectTransform>().rect.width/2 + (_boardSize/2*80) - i * 80,_backgroundPanel.GetComponent<RectTransform>().rect.height/2 + (_boardSize/2*80) - b * 80);;
                     //add buttons that are surrounding boxes to list within the box prefab.
-                    //_box.GetComponent<BoxScript>().boxLines(rowButtons[b.ToString() + i.ToString()]); 
-                    //_box.GetComponent<BoxScript>().boxLines(rowButtons[(b+1).ToString() + i.ToString()]);
-                   // _box.GetComponent<BoxScript>().boxLines(colButtons[b.ToString() + i.ToString()]);
-                   // _box.GetComponent<BoxScript>().boxLines(colButtons[b.ToString() + (i + 1).ToString()]);
-                   // _box.tag = "unchecked";//tag box that it hasnt been taken for points.
+                    _box.GetComponent<BoxScript>().boxLines(_gameBoard.rowButtons[b.ToString() + i.ToString() + "r"]); 
+                    _box.GetComponent<BoxScript>().boxLines(_gameBoard.rowButtons[(b+1).ToString() + i.ToString()+ "r"]);
+                    _box.GetComponent<BoxScript>().boxLines(_gameBoard.colButtons[b.ToString() + i.ToString()+ "c"]);
+                    _box.GetComponent<BoxScript>().boxLines(_gameBoard.colButtons[b.ToString() + (i + 1).ToString()+ "c"]);
+                    _box.tag = "unchecked";//tag box that it hasnt been taken for points.
                     _box.name = (b.ToString() + i.ToString() + "b"); //name box given x,y coords on board.
-                    //boxes.Add(_box); //add box to boxes list.
+                    _gameBoard.boxes.Add(_box); //add box to boxes list.
                 }
             }
         }
         //ggText.GetComponent<RectTransform>().SetAsLastSibling(); // make the gg text be on top of everything else. 
     }
+
+    
 
     public void ButtonClicked(Button _b, bool _row){ //function is called when row or column button is pressed, passes button pressed & bool for whether it is a row or col button.
         bool alreadyClicked = false; //temp holds if it has been pressed in the past
@@ -122,9 +124,74 @@ public class GameScript : MonoBehaviour
             ColorBlock m_tempColor = _b.GetComponent<Button>().colors; //make temporary color block to change alpha of button to 100%
             m_tempColor.normalColor = _b.GetComponent<Image>().color; //set normal color to what color button should be.
             _b.GetComponent<Button>().colors = m_tempColor; //set buttons color block to temp color block
-            
-            //CheckBoxes(); //check if box needs to be filled in. 
+
+            CheckBoxes(); //check if box needs to be filled in. 
         }
+    }
+    private void CheckBoxes(){
+        
+        bool m_didCheck = false; //holds whether there are any unclaimed boxes left
+        bool[] m_clicked = new bool[4]; //temp array to hold bools of whether its been clicked.
+        bool m_pointGained = false; //holds whether you scored a point or not, basically to change turns, if point gained dont change turn.
+        
+        foreach(GameObject box in _gameBoard.boxes ){ //go through list of boxes in game
+            if(box.tag == "unchecked"){ //make sure box hasnt been claimed already
+                List<Button> temp =  box.GetComponent<BoxScript>().buttonList; //temp list of buttons surrounding box
+                int counter = 0; //counter to distinguish buttons between row and column
+                foreach(Button but in temp){
+                    if(counter < 2){ //first two buttons stored on box are always ROW buttons
+                        //check and add whether button was m_clicked to temp list
+                        if(_gameBoard.rowClicked[but.name]){
+                            m_clicked[counter] = true; 
+                        }else{
+                             m_clicked[counter] = false ;
+                        }
+                    }else if (counter < 4){//second two buttons stored on box are always COL buttons
+                        //check and add whether button was m_clicked to temp list
+                        if(_gameBoard.colClicked[but.name]){
+                            m_clicked[counter] = true; 
+                        }else{ 
+                            m_clicked[counter] = false;
+                        }
+
+                    }else{ //button shouldnt exist
+                        Debug.Log("button shouldnt exist");
+                    }   
+                    counter ++;
+                    
+                }
+                if(m_clicked[0] && m_clicked[1] && m_clicked[2] && m_clicked[3]){ // if all buttons in temp m_clicked array were pressed do this.
+                    box.GetComponent<BoxScript>().ButtonSurrounded(_firstPlayersTurn); //pass to box script to change its colour and depending on whos turn it is.
+                    m_pointGained = true; //point is gained
+                    //add points to whoevers turns score.
+                    if(_firstPlayersTurn){ 
+                        //p1Score++;
+                    }else if(!_firstPlayersTurn){
+                        //p2Score++;
+                    }
+                    
+                }
+                
+            }
+        }
+        //if point isnt gained change
+        if(!m_pointGained){
+                _firstPlayersTurn = !_firstPlayersTurn;
+        }
+        //check if there are any unchecked boxes left
+        foreach(GameObject box in _gameBoard.boxes ){
+            if(box.tag == "unchecked"){
+                m_didCheck = true;
+                break;
+            }
+        } 
+        //if no more unchecked boxes left end game.
+        if(!m_didCheck && _currentGamestate != GAMESTATE.GAMEOVER){
+            _currentGamestate = GAMESTATE.GAMEOVER;
+        }else{
+            m_didCheck = false;
+        }
+        
     }
 
     // -------------------------CLASSES ---------------------------------------
